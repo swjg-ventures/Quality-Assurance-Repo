f929b5af86ea33e1175aeafbccf9781aace99118
Rajesh <rajesh.kumar@beepermd.com>
2020-06-16 08:19:48 +0530

    Changes

--- AutoBrain/src/test/java/com/maven/testcases/DemoTest.java
+++ AutoBrain/src/test/java/com/maven/testcases/DemoTest.java
@@ -10,21 +10,21 @@ import com.pages.VehicleProfile;
 
 public class DemoTest extends Base {
 
-//	@Test(priority = 1)
+	@Test(priority = 1)
 	public void VerifyPersonalAccountRegistration() throws Exception {
 		Signup a = new Signup();
 		a.signup("personal");
 
 	}
 
-//	@Test(priority = 2)
+	@Test(priority = 2)
 	public void VerifyBusinessAccountRegistration() throws Exception {
 		Signup a = new Signup();
 		a.signup("business");
 
 	}
 
-//	@Test(priority = 3)
+	@Test(priority = 3)
 	public void VerifySignupWithPrepaidDevice() throws Exception {
 		SignupWithManualInvoice order = new SignupWithManualInvoice();
 		order.signupWithManualInvoice("personal");
@@ -36,7 +36,7 @@ public class DemoTest extends Base {
 		l.login("john@example.com", "welcome");
 	}
 
-//	@Test(priority = 5)
+	@Test(priority = 5)
 	public void verifyUpdateVehicleProfile() throws Exception {
 		VehicleProfile l = new VehicleProfile();
 		l.VehicleProfileInfo();
